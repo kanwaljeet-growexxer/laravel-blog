b6f23c804d400cb7cb1a31e50049955db29579b8
kanwaljeet-growexxer <kanwaljeet.singh@growexx.com>
2022-03-14 20:43:33 +0530

    feat(delete post): added ability to delete post

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -34,10 +34,10 @@
                                 <a href="{{ url('post/' . $item->id) }}" title="View Post"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
                                 <a href="{{ url('/post/' . $item->id . '/edit') }}" title="Edit Post"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
 
-                                <form method="POST" action="{{ url('/admin/posts' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                <form method="POST" action="{{ url('/post' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
                                     {{ method_field('DELETE') }}
                                     {{ csrf_field() }}
-                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete Post" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete Post" onclick="return confirm('Do you want to delete this post?')"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
                                 </form>
                             </td>
                         </tr>

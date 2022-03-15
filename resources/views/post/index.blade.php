3676bda30c4f8502f328617fbe4a291743789866
kanwaljeet-growexxer <96059444+kanwaljeet-growexxer@users.noreply.github.com>
2022-03-15 17:51:10 +0530

    fix(list posts): fix the design of list post page (#9)

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -8,42 +8,66 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <x-success-message />
-               
-                <div class="flex items-center mt-4">
-                        <a href="{{ url('/post/create') }}" class="btn btn-success btn-sm">
-                            <x-button class="ml-3">
-                                {{ __('Add New Post') }}
-                            </x-button>
-                        </a>
-                </div>
-                        <br/>
-                        <br/>
-                        
-                <table class="table-auto">
-                    <thead>
-                        <tr>
-                            <th>#</th><th>Title</th><th>Content</th><th>Category</th><th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($posts as $item)
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $item->title }}</td><td>{{ $item->description }}</td><td>{{ $item->category }}</td>
-                            <td>
-                                <a href="{{ url('post/' . $item->id) }}" title="View Post"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
-                                <a href="{{ url('/post/' . $item->id . '/edit') }}" title="Edit Post"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="flex flex-col">
+                        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                <div class="mb-4">
+                                    <a href="{{ url('/post/create') }}" class="btn btn-success btn-sm">
+                                        <x-button class="ml-3">
+                                            {{ __('Add New Post') }}
+                                        </x-button>
+                                    </a>
+                                </div>
+                                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                    <table class="min-w-full divide-y divide-gray-200">
+                                        <thead class="bg-gray-50">
+                                        <tr>
+                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                <span class="sr-only">Title</span>
+                                            </th>
+                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                <span class="sr-only">Description</span>
+                                            </th>
+                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                <span class="sr-only">Category</span>
+                                            </th>
+                                            <th scope="col" class="relative px-6 py-3">
+                                                <span class="sr-only">Actions</span>
+                                            </th>
+                                        </tr>
+                                        </thead>
+                                        <tbody class="bg-white divide-y divide-gray-200">
+                                        @foreach($posts as $item)
+                                        <tr>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                {{ $item->title }}
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                {{ $item->description }}
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                {{ $item->category }}
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                <a href="{{ url('post/' . $item->id) }}" title="View Post"class="text-indigo-600 hover:text-indigo-900">View</a>
+                                                <a href="{{ url('/post/' . $item->id . '/edit') }}" title="Edit Post"class="text-indigo-600 hover:text-indigo-900">Edit</a>
 
-                                <form method="POST" action="{{ url('/post' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
-                                    {{ method_field('DELETE') }}
-                                    {{ csrf_field() }}
-                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete Post" onclick="return confirm('Do you want to delete this post?')"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                                                <form method="POST" action="{{ url('/post' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                                    {{ method_field('DELETE') }}
+                                                    {{ csrf_field() }}
+                                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete Post" onclick="return confirm('Do you want to delete this post?')">Delete</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
